ddde0188461cbd3eca3485d2b345eb5c40fd8f5f
Jeremy-Leckning <49643741+Jeremy-Leckning@users.noreply.github.com>
2021-10-18 13:46:35 +1100

    Added files

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,47 @@
+var vg_1 = "js/fatality_trend.json";
+vegaEmbed("#fatality_trend", vg_1)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_2 = "js/map_visualisation.json";
+vegaEmbed("#map_visualisation", vg_2)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_3 = "js/map_visualisation2.json";
+vegaEmbed("#map_visualisation2", vg_3)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_4 = "js/bar_chart.json";
+vegaEmbed("#bar chart", vg_4)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+// var vg_1 = "cloromap.json";
+// vegaEmbed("#worldmap", vg_1)
+//   .then(function (result) {})
+//   .catch(console.error);
+
+// var vg_1 = "cloromap.json";
+// vegaEmbed("#worldmap", vg_1)
+//   .then(function (result) {})
+//   .catch(console.error);
+
+// var vg_1 = "cloromap.json";
+// vegaEmbed("#worldmap", vg_1)
+//   .then(function (result) {})
+//   .catch(console.error);
+
+// var vg_1 = "cloromap.json";
+// vegaEmbed("#worldmap", vg_1)
+//   .then(function (result) {})
+//   .catch(console.error);
