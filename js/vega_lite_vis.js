3a43841ac9fbfe0b4a10610683b95ae351a51e95
Jeremy-Leckning <49643741+Jeremy-Leckning@users.noreply.github.com>
2021-10-18 16:05:04 +1100

    Re-uploading files

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,26 +1,26 @@
 var vg_1 = "js/fatality_trend.json";
-vegaEmbed("#fatality_trend", vg_1)
+vegaEmbed("#fatality_trend", vg_1, { actions: false })
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
   .catch(console.error);
 
 var vg_2 = "js/map_visualisation.json";
-vegaEmbed("#map_visualisation", vg_2)
+vegaEmbed("#map_visualisation", vg_2, { actions: false })
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
   .catch(console.error);
 
 var vg_3 = "js/map_visualisation2.json";
-vegaEmbed("#map_visualisation2", vg_3)
+vegaEmbed("#map_visualisation2", vg_3, { actions: false })
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
   .catch(console.error);
 
 var vg_4 = "js/bar_chart.json";
-vegaEmbed("#bar_chart", vg_4)
+vegaEmbed("#bar_chart", vg_4, { actions: false })
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
